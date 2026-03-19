66b3034c289157908e586bb9c57c50b361a35a5d
foll0wer <151058944+foll0wer@users.noreply.github.com>
2026-03-19 10:15:10 +0100

    Update PageGrapes.kt lang immmmp

--- app/src/main/java/com/mouton/openwinemer/ui/edit/PageGrapes.kt
+++ app/src/main/java/com/mouton/openwinemer/ui/edit/PageGrapes.kt
@@ -6,6 +6,9 @@ import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.mouton.openwinemer.data.model.WineEntity
+import androidx.compose.ui.res.stringResource
+import com.mouton.openwinemer.R
+
 
 @Composable
 fun PageGrapes(
