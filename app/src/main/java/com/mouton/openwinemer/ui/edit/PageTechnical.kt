cda625a16c97cd402555dfe22dc14106a6afc5d9
foll0wer <151058944+foll0wer@users.noreply.github.com>
2026-03-19 10:15:45 +0100

    LA LANGUE L'IMPORT

--- app/src/main/java/com/mouton/openwinemer/ui/edit/PageTechnical.kt
+++ app/src/main/java/com/mouton/openwinemer/ui/edit/PageTechnical.kt
@@ -6,6 +6,8 @@ import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.mouton.openwinemer.data.model.WineEntity
+import androidx.compose.ui.res.stringResource
+import com.mouton.openwinemer.R
 
 @Composable
 fun PageTechnical(
